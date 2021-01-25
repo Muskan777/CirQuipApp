8a616a025a73ce4d748af5765bf21f96ff2a0788
Muskan Paryani <muskanparyani007@gmail.com>
2021-01-26 01:22:53 +0530

    minor fixes

--- CirQuip/screens/ChatWithAdmin.jsx
+++ CirQuip/screens/ChatWithAdmin.jsx
@@ -7,32 +7,30 @@ export function ChatWithAdmin(props) {
   const [socket, setSocket] = useState(io("http://192.168.43.192:3000"));
 
   useEffect(() => {
-    if (!props.route.params.admin) {
-      setMessages([
-        {
-          _id: "Admin",
-          text: "Hello!!",
-          createdAt: new Date(),
-          user: {
-            _id: 1,
-            name: "React Native",
-            avatar: "https://placeimg.com/140/140/any",
-          },
+    setMessages([
+      {
+        _id: "Admin",
+        text: "Hello!!",
+        createdAt: new Date(),
+        user: {
+          _id: 1,
+          name: "React Native",
+          avatar: "https://placeimg.com/140/140/any",
         },
-      ]);
-    }
+      },
+    ]);
     console.log(socket.id);
-  //   props.route.params.admin
-  //     ? socket.emit("new user", "Admin")
-  //     : socket.emit("new user", props.route.params.email);
-   }, []);
+    //   props.route.params.admin
+    //     ? socket.emit("new user", "Admin")
+    //     : socket.emit("new user", props.route.params.email);
+  }, []);
 
   const onSend = useCallback((messages = []) => {
     setMessages(previousMessages =>
       GiftedChat.append(previousMessages, messages)
     );
     console.log("chat message", messages);
-   // socket.emit("send message", messages);
+    // socket.emit("send message", messages);
   }, []);
 
   // socket.on("new message", function (messages) {
