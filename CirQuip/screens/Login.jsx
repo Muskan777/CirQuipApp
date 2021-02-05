281e1863cdc9a894c10ff6fbc92ba920b0c3e532
NikitaTipule <67597066+NikitaTipule@users.noreply.github.com>
2021-02-06 00:19:01 +0530

    fix :changed UI of signUp and login page

--- CirQuip/screens/Login.jsx
+++ CirQuip/screens/Login.jsx
@@ -7,8 +7,10 @@ import {
   TextInput,
   TouchableOpacity,
   Alert,
+  ScrollView,
 } from "react-native";
 import AsyncStorage from "@react-native-async-storage/async-storage";
+import SignUp1 from "../components/SignUp1";
 export default class Login extends React.Component {
   constructor(props) {
     super(props);
@@ -20,7 +22,7 @@ export default class Login extends React.Component {
       college: "COEP",
       password2: "123",
       role: "Student",
-      toggleSignUp: false,
+      toggleSignUp: true,
     };
   }
   async handleLogin() {
@@ -71,9 +73,9 @@ export default class Login extends React.Component {
   }
   render() {
     return (
-      <>
+      <ScrollView>
         <View style={styles.container}>
-          <Text style={styles.logo}>CirQuip</Text>
+          <SignUp1 />
           {this.state.toggleSignUp ? (
             <>
               <View style={styles.inputView}>
@@ -81,29 +83,32 @@ export default class Login extends React.Component {
                   value={this.state.name}
                   style={styles.inputText}
                   placeholder="Name..."
-                  placeholderTextColor="#003f5c"
+                  placeholderTextColor="grey"
                   onChangeText={text => this.setState({ name: text })}
                 />
               </View>
+              <View style={styles.line}></View>
               <View style={styles.inputView}>
                 <TextInput
                   value={this.state.college}
                   style={styles.inputText}
                   placeholder="College..."
-                  placeholderTextColor="#003f5c"
+                  placeholderTextColor="grey"
                   onChangeText={text => this.setState({ college: text })}
                 />
               </View>
+              <View style={styles.line}></View>
               <View style={styles.inputView}>
                 <TextInput
                   value={this.state.email}
                   style={styles.inputText}
                   textContentType={"emailAddress"}
                   placeholder="Email..."
-                  placeholderTextColor="#003f5c"
+                  placeholderTextColor="grey"
                   onChangeText={text => this.setState({ email: text })}
                 />
               </View>
+              <View style={styles.line}></View>
               <View style={styles.inputView}>
                 <TextInput
                   maxLength={10}
@@ -111,31 +116,34 @@ export default class Login extends React.Component {
                   textContentType={"telephoneNumber"}
                   keyboardType={"phone-pad"}
                   style={styles.inputText}
-                  placeholder="Phone"
-                  placeholderTextColor="#003f5c"
+                  placeholder="Mobile no."
+                  placeholderTextColor="grey"
                   onChangeText={text => this.setState({ phone: text })}
                 />
               </View>
+              <View style={styles.line}></View>
               <View style={styles.inputView}>
                 <TextInput
                   value={this.state.password}
                   secureTextEntry
                   style={styles.inputText}
                   placeholder="Password..."
-                  placeholderTextColor="#003f5c"
+                  placeholderTextColor="grey"
                   onChangeText={text => this.setState({ password: text })}
                 />
               </View>
+              <View style={styles.line}></View>
               <View style={styles.inputView}>
                 <TextInput
                   value={this.state.password2}
                   secureTextEntry
                   style={styles.inputText}
                   placeholder="Confirm Password..."
-                  placeholderTextColor="#003f5c"
+                  placeholderTextColor="grey"
                   onChangeText={text => this.setState({ password2: text })}
                 />
               </View>
+              <View style={styles.line}></View>
               <TouchableOpacity
                 style={styles.loginBtn}
                 onPress={() => this.handleSignUp()}
@@ -145,28 +153,35 @@ export default class Login extends React.Component {
               <TouchableOpacity onPress={() => this.toggleSignUp()}>
                 <Text style={styles.loginText}>Sign In</Text>
               </TouchableOpacity>
+              <View style={styles.already1}>
+              <Text style = {styles.already}>Already have an account?</Text>
+              <TouchableOpacity onPress={()=>this.toggleSignUp()}><Text style= {styles.already}> Sign In</Text></TouchableOpacity>
+              </View>
             </>
           ) : (
-            <>
+            <><View style = {styles.loginfield}></View>
               <View style={styles.inputView}>
                 <TextInput
                   value={this.state.email}
                   style={styles.inputText}
                   placeholder="Email..."
-                  placeholderTextColor="#003f5c"
+                  placeholderTextColor="grey"
                   onChangeText={text => this.setState({ email: text })}
                 />
               </View>
+              <View style={styles.line}></View>
               <View style={styles.inputView}>
                 <TextInput
                   secureTextEntry
                   value={this.state.password}
                   style={styles.inputText}
                   placeholder="Password..."
-                  placeholderTextColor="#003f5c"
+                  placeholderTextColor="grey"
                   onChangeText={text => this.setState({ password: text })}
                 />
               </View>
+              <View style={styles.line}></View>
+              
               <TouchableOpacity
                 onPress={() =>
                   Alert.alert(
@@ -186,10 +201,11 @@ export default class Login extends React.Component {
               <TouchableOpacity onPress={() => this.toggleSignUp()}>
                 <Text style={styles.loginText}>Signup</Text>
               </TouchableOpacity>
+              
             </>
           )}
         </View>
-      </>
+      </ScrollView>
     );
   }
 }
@@ -197,7 +213,7 @@ export default class Login extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#003f5c",
+    backgroundColor: "white",
     alignItems: "center",
     justifyContent: "center",
   },
@@ -208,33 +224,88 @@ const styles = StyleSheet.create({
     marginBottom: 40,
   },
   inputView: {
-    width: "80%",
-    backgroundColor: "#465881",
-    borderRadius: 25,
-    height: 50,
-    marginBottom: 20,
-    justifyContent: "center",
-    padding: 20,
+
+      color: "rgba(159,159,159,1)",
+      fontSize: 20,
+      fontWeight: "400",
+      fontStyle: "normal",
+      // sfontFamily: "Segoe UI",
+      textAlign: "center",
+      paddingVertical: 0,
+      marginTop: 9,
+      marginBottom: 1,
+    
   },
   inputText: {
-    height: 50,
-    color: "white",
+      color: "black",
+      fontSize: 20,
+      fontWeight: "400",
+      fontStyle: "normal",
+      // fontFamily: "Segoe UI",
+      textAlign: "center",
+      paddingVertical: 0,
+      marginTop: 9,
+      marginBottom: 1,
+    
   },
   forgot: {
     color: "white",
     fontSize: 11,
   },
   loginBtn: {
-    width: "80%",
-    backgroundColor: "#fb5b5a",
+    width: "40%",
+    backgroundColor: "rgba(54, 181, 165, 0.6313725490196078)",
     borderRadius: 25,
     height: 50,
     alignItems: "center",
     justifyContent: "center",
     marginTop: 40,
-    marginBottom: 10,
+    marginBottom: 0,
+    shadowColor: "black",
+    elevation: 3,
+    shadowOpacity: 0.32941176470588235,
+    shadowOffset: {
+      width: 0,
+      height: 0,
+    },
+    shadowRadius: 20,
+    
   },
   loginText: {
     color: "white",
+    fontSize:20,
+    fontWeight: '700'
+    // marginBottom: 100,
   },
+  line: {
+    width: "65%",
+    backgroundColor: "rgba(0,0,0,0.5)",
+    height: 1,
+    marginBottom: 11,
+    shadowColor: "rgb(0,0,0)",
+    shadowOpacity: 0.4,
+    shadowOffset: {
+      width: 0,
+      height: 3.5,
+    },
+    shadowRadius: 6,
+    elevation: 4,
+  },
+  already: {
+    color: "rgba(39, 40, 51, 0.7)",
+    fontSize: 14,
+    fontWeight: "100",
+    fontStyle: "normal",
+    // fontFamily: "Avenir",
+    lineHeight: 20,
+    marginTop: 0,
+    marginBottom: 50,
+  },
+  loginfield: {
+    marginTop: 120,
+  },
+  already1: {
+    flexDirection:'row',
+    alignItems:'center'
+  }
 });
