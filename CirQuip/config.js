7710705ea304b4f6806a8246008bf334d6cf8f9c
vasusharma7 <vasusharma2017@outlook.com>
2021-02-11 00:24:16 +0530

    chore: changed hosted URL

--- CirQuip/config.js
+++ CirQuip/config.js
@@ -8,13 +8,8 @@ console.log(uri);
 console.log(Constants.default.manifest.hostUri);
 global.config = {
   debug: true,
-  host:
-    "http://" + Constants.default.manifest.hostUri.split(":")[0] + ":3000/api",
+  host: __DEV__
+    ? "http://" + Constants.default.manifest.hostUri.split(":")[0] + ":3000/api"
+    : "http://15.207.85.98/api",
   admin: "vasusharma656@gmail.com",
 };
-//global.config = {
-//test: "http://a053e0e96ed0.ngrok.io",
-//debug: true,
-//host: "http://a053e0e96ed0.ngrok.io/api",
-//admin: "vasusharma656@gmail.com",
-//};
