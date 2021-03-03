dd02bfb2b6f14fb7476791c10725bf562c41e6ba
vasusharma7 <vasusharma2017@outlook.com>
2021-03-03 17:31:02 +0530

    chore: changing socket URI

--- CirQuip/config.js
+++ CirQuip/config.js
@@ -1,8 +1,8 @@
 import * as Constants from "expo-constants";
 
-//socketURL: "http://15.207.85.98:3000",
+//socketURL: "http://172.20.10.2:3000",
 global.config = {
-  socketURL: "http://172.20.10.2:3000",
+  socketURL: "http://15.207.85.98:3000",
   debug: true,
   host: __DEV__
     ? "http://" + Constants.default.manifest.hostUri.split(":")[0] + ":3000/api"
