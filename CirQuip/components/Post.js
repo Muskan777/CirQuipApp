8223c7eb144a5de0c2fdfe9f9e9e5ee95c73db79
Shreya <shreyakvaidya@gmail.com>
2021-03-21 22:07:26 +0530

    fix: resolved post likes bug

--- CirQuip/components/Post.js
+++ CirQuip/components/Post.js
@@ -1,21 +1,9 @@
 import React, { useState, useEffect } from "react";
 import Hyperlink from "react-native-hyperlink";
-import {
-  StyleSheet,
-  View,
-  Text,
-  Image,
-  TouchableHighlight,
-  TouchableOpacity,
-  Dimensions,
-  Share,
-  Modal,
-  Alert,
-} from "react-native";
+import { StyleSheet, View, Text, Image, Dimensions, Alert } from "react-native";
 import { Card } from "react-native-material-cards";
 import axios from "axios";
 import AsyncStorage from "@react-native-async-storage/async-storage";
-import { Dialog, Portal, Button, Menu } from "react-native-paper";
 import "../config";
 import Clipboard from "expo-clipboard";
 import * as Linking from "expo-linking";
@@ -25,33 +13,25 @@ import {
   Entypo,
 } from "@expo/vector-icons";
 import { Video } from "expo-av";
-import Loader from "../screens/Loader";
-import Carousel, { Pagination } from "react-native-snap-carousel"; // Version can be specified in package.json
+import Carousel, { Pagination } from "react-native-snap-carousel";
 import { TouchableWithoutFeedback } from "react-native-gesture-handler";
 import Toast from "react-native-simple-toast";
 const SLIDER_WIDTH = (Dimensions.get("window").width * 9.2) / 10;
 const ITEM_WIDTH = Math.round(SLIDER_WIDTH * 1);
-export default function PostCarousel({
+export default function Post({
   createdAt,
   caption,
   comments,
   content,
   likes,
-  saves,
-  shares,
   name,
   role,
   navigation,
   postId,
-  postIndex,
-  onCommentClick,
   taggedUsers,
   id,
-  onRefresh,
   skill,
   club,
-  interest,
-  userEmail,
 }) {
   const date =
     createdAt.substr(8, 2) +
@@ -76,23 +56,11 @@ export default function PostCarousel({
   } else {
     var time = localhour + ":" + localmin + " AM";
   }
-  const [currentLikes, setCurrentLikes] = useState(likes);
-  const [currentSaves, setCurrentSaves] = useState(saves);
-  const [currentShares, setCurrentShares] = useState(shares);
   const [liked, setLiked] = useState(false);
   const [saved, setSaved] = useState(false);
   const [activeSlide, setActiveSlide] = useState(0);
-  const [shared, setShared] = useState(false);
-  const [URL, setURL] = useState(null);
-  const [allowDelete, setAllowDelete] = useState(null);
-  const [visible, setVisible] = useState(false);
-  const [visibleReport, setVisibleReport] = useState(false);
-  const [loading, setLoading] = useState(false);
-  const [deleted, setDeleted] = useState(false);
-  const [visibleMenu, setVisibleMenu] = React.useState(false);
   const [full, setfull] = React.useState(false);
   const [userImage, setUserImage] = useState("");
-  let key = 0;
   let usersTagged = [];
   let usersTaggedId = [];
 
@@ -116,45 +84,6 @@ export default function PostCarousel({
       });
   };
 
-  const handleDialog = () => {
-    setVisibleMenu(false);
-    setVisible(!visible);
-  };
-
-  const handleReportPost = async () => {
-    let token = await AsyncStorage.getItem("cirquip-auth-token");
-
-    axios
-      .post(
-        `${global.config.host}/post/reportPost`,
-        {
-          URL: URL,
-          email: userEmail,
-          name: name,
-          caption: caption,
-        },
-        {
-          headers: {
-            "cirquip-auth-token": token,
-          },
-        }
-      )
-      .then(res => {
-        console.log("Mail Sent!");
-      })
-      .catch(err => {
-        console.log("Error in sending mail");
-      });
-    Toast.show("Post Reported!", Toast.SHORT, ["UIAlertController"]);
-    setVisibleMenu(false);
-    setVisibleReport(!visibleReport);
-  };
-
-  const handleReportDialog = () => {
-    setVisibleMenu(false);
-    setVisibleReport(!visibleReport);
-  };
-  const handleMenu = () => setVisibleMenu(!visibleMenu);
   useEffect(() => {
     fetchLikedPosts();
     fetchUser();
@@ -162,13 +91,6 @@ export default function PostCarousel({
 
   const fetchLikedPosts = async () => {
     let token = await AsyncStorage.getItem("cirquip-auth-token");
-    let userId = await AsyncStorage.getItem("user");
-    let userEmail = await AsyncStorage.getItem("email");
-    const initialUrl = await Linking.getInitialURL();
-    setURL(initialUrl);
-    if (userId === id || global.config.admin.includes(userEmail)) {
-      setAllowDelete(true);
-    }
     axios
       .get(`${global.config.host}/user/getLikedPosts`, {
         headers: {
@@ -194,133 +116,8 @@ export default function PostCarousel({
             }
           }
         }
-        let tempShared = false;
-        if (res.data.sharedPosts) {
-          for (var i = 0; i < res.data.sharedPosts.length; i++) {
-            if (res.data.sharedPosts[i] === postId) {
-              tempShared = true;
-              break;
-            }
-          }
-        }
         setLiked(tempLiked);
         setSaved(tempSaved);
-        setShared(tempShared);
-      })
-      .catch(e => console.log(e));
-  };
-  const handleLikes = async () => {
-    let token = await AsyncStorage.getItem("cirquip-auth-token");
-
-    axios
-      .patch(
-        `${global.config.host}/post/likePost`,
-        {
-          id: postId,
-          liked: liked,
-        },
-        {
-          headers: {
-            "cirquip-auth-token": token,
-          },
-        }
-      )
-      .then(res => {
-        setCurrentLikes(res.data.likes);
-        setLiked(res.data.liked);
-        onRefresh(true);
-      })
-      .catch(e => console.log(e));
-  };
-
-  const handleSave = async () => {
-    let token = await AsyncStorage.getItem("cirquip-auth-token");
-
-    axios
-      .patch(
-        `${global.config.host}/post/savePost`,
-        {
-          id: postId,
-          saved: saved,
-        },
-        {
-          headers: {
-            "cirquip-auth-token": token,
-          },
-        }
-      )
-      .then(res => {
-        setCurrentSaves(res.data.saves);
-        setSaved(res.data.saved);
-      })
-      .catch(e => console.log(e));
-  };
-  // const handleShare = async () => {
-  //   let token = await AsyncStorage.getItem("cirquip-auth-token");
-  //   axios
-  //     .patch(
-  //       `${global.config.host}/post/sharePost`,
-  //       {
-  //         id: postId,
-  //         shared: shared,
-  //       },
-  //       {
-  //         headers: {
-  //           "cirquip-auth-token": token,
-  //         },
-  //       }
-  //     )
-  //     .then(res => {
-  //       setCurrentShares(res.data.shares);
-  //       setShared(res.data.shared);
-  //     })
-  //     .catch(e => console.log(e));
-  // };
-  const handleShare = async () => {
-    // let redirectUrl = Linking.createURL("posts", {
-    //   queryParams: { id: postId },
-    // });
-    // console.log(redirectUrl);
-    let redirectUrl = `https://app.cirquip.com/posts?id=${postId}`;
-    try {
-      const result = await Share.share({
-        message: `Check out ${name}'s post! \n${caption}\n${redirectUrl}`,
-      });
-      if (result.action === Share.sharedAction) {
-        if (result.activityType) {
-          // shared with activity type of result.activityType
-        } else {
-          // shared
-        }
-      } else if (result.action === Share.dismissedAction) {
-        // dismissed
-      }
-    } catch (error) {
-      alert(error.message);
-    }
-  };
-
-  const handleDelete = async () => {
-    setLoading(true);
-    let token = await AsyncStorage.getItem("cirquip-auth-token");
-    axios
-      .post(
-        `${global.config.host}/post/deletePost`,
-        {
-          id: postId,
-        },
-        {
-          headers: {
-            "cirquip-auth-token": token,
-          },
-        }
-      )
-      .then(res => {
-        onRefresh(true);
-        setDeleted(true);
-        setLoading(false);
-        handleDialog();
-        Toast.show("Post Deleted!", Toast.SHORT, ["UIAlertController"]);
       })
       .catch(e => console.log(e));
   };
@@ -466,23 +263,6 @@ export default function PostCarousel({
             </View>
           )}
         </View>
-        <View style={styles.closeIcon}>
-          <Menu
-            visible={visibleMenu}
-            onDismiss={handleMenu}
-            anchor={
-              <TouchableOpacity onPress={handleMenu}>
-                <FontAwesome name="ellipsis-v" style={styles.threedots} />
-              </TouchableOpacity>
-            }
-          >
-            {allowDelete ? (
-              <Menu.Item onPress={handleDialog} title="Delete Post" />
-            ) : (
-              <Menu.Item onPress={handleReportDialog} title="Report Post" />
-            )}
-          </Menu>
-        </View>
       </View>
 
       <View style={styles.postCaption}>
@@ -571,185 +351,32 @@ export default function PostCarousel({
       </View>
       <View style={styles.respnseButtons}>
         <View style={styles.IconContainer}>
-          <TouchableOpacity onPress={handleLikes}>
-            {liked ? (
-              <FontAwesome name="heart" size={30} style={styles.Icons} />
-            ) : (
-              <FontAwesome name="heart-o" size={30} style={styles.Icons} />
-            )}
-          </TouchableOpacity>
-          <Text style={styles.TextStyle}>{currentLikes}</Text>
+          {liked ? (
+            <FontAwesome name="heart" size={30} style={styles.Icons} />
+          ) : (
+            <FontAwesome name="heart-o" size={30} style={styles.Icons} />
+          )}
+          <Text style={styles.TextStyle}>{likes}</Text>
         </View>
         <View style={styles.IconContainer}>
-          <TouchableOpacity onPress={() => onCommentClick(postIndex, postId)}>
-            <MaterialCommunityIcons
-              name="comment-processing"
-              size={30}
-              style={styles.Icons}
-            />
-          </TouchableOpacity>
+          <MaterialCommunityIcons
+            name="comment-processing"
+            size={30}
+            style={styles.Icons}
+          />
           <Text style={styles.TextStyle}>{comments.length}</Text>
         </View>
         <View style={styles.IconContainer}>
-          <TouchableOpacity onPress={handleSave}>
-            {saved ? (
-              <FontAwesome name="bookmark" size={30} style={styles.Icons} />
-            ) : (
-              <FontAwesome name="bookmark-o" size={30} style={styles.Icons} />
-            )}
-          </TouchableOpacity>
+          {saved ? (
+            <FontAwesome name="bookmark" size={30} style={styles.Icons} />
+          ) : (
+            <FontAwesome name="bookmark-o" size={30} style={styles.Icons} />
+          )}
         </View>
         <View style={styles.IconContainer}>
-          <TouchableOpacity onPress={handleShare}>
-            {shared ? (
-              <FontAwesome name="share-square" size={30} style={styles.Icons} />
-            ) : (
-              <FontAwesome
-                name="share-square-o"
-                size={30}
-                style={styles.Icons}
-              />
-            )}
-          </TouchableOpacity>
-          {/* <Text style={styles.TextStyle}>{currentShares}</Text> */}
+          <FontAwesome name="share-square" size={30} style={styles.Icons} />
         </View>
       </View>
-      <Modal animationType="fade" transparent={true} visible={visible}>
-        <View
-          style={{
-            height: 175,
-            width: 350,
-            alignContent: "center",
-            position: "absolute",
-            alignSelf: "center",
-            backgroundColor: "#ffff",
-            borderRadius: 10,
-            shadowOpacity: 1,
-            elevation: 6,
-            top: 350,
-          }}
-        >
-          <View
-            style={{
-              display: "flex",
-              flexDirection: "column",
-              padding: 20,
-              flex: 1,
-            }}
-          >
-            <Text
-              style={{
-                fontWeight: "bold",
-                fontSize: 20,
-                color: "#B11B1B",
-                alignSelf: "center",
-                marginBottom: 10,
-              }}
-            >
-              Confirm Deletion
-            </Text>
-
-            {loading ? (
-              <Loader />
-            ) : (
-              <View>
-                {!deleted ? (
-                  <Text
-                    style={{
-                      fontWeight: "bold",
-                      fontSize: 18,
-                      color: "gray",
-                      alignSelf: "center",
-                      margin: 10,
-                    }}
-                  >
-                    Are you sure you want to delete this post?
-                  </Text>
-                ) : null}
-              </View>
-            )}
-
-            {!deleted ? (
-              <View
-                style={{
-                  flexDirection: "row",
-                  justifyContent: "flex-end",
-                }}
-              >
-                <Button onPress={handleDialog} color="gray" fontSize="15">
-                  Back
-                </Button>
-                <Button onPress={handleDelete} color="#B11B1B" fontSize="15">
-                  Delete
-                </Button>
-              </View>
-            ) : null}
-          </View>
-        </View>
-      </Modal>
-      <Modal animationType="fade" transparent={true} visible={visibleReport}>
-        <View
-          style={{
-            height: 175,
-            width: 350,
-            alignContent: "center",
-            position: "absolute",
-            alignSelf: "center",
-            backgroundColor: "#ffff",
-            borderRadius: 10,
-            shadowOpacity: 1,
-            elevation: 6,
-            top: 350,
-          }}
-        >
-          <View
-            style={{
-              display: "flex",
-              flexDirection: "column",
-              padding: 20,
-              flex: 1,
-            }}
-          >
-            <Text
-              style={{
-                fontWeight: "bold",
-                fontSize: 20,
-                color: "#B11B1B",
-                alignSelf: "center",
-                marginBottom: 10,
-              }}
-            >
-              Report Post
-            </Text>
-            <View>
-              <Text
-                style={{
-                  fontWeight: "bold",
-                  fontSize: 18,
-                  color: "gray",
-                  alignSelf: "center",
-                  margin: 10,
-                }}
-              >
-                Are you sure you want to report this post?
-              </Text>
-            </View>
-            <View
-              style={{
-                flexDirection: "row",
-                justifyContent: "flex-end",
-              }}
-            >
-              <Button onPress={handleReportDialog} color="gray" fontSize="15">
-                Back
-              </Button>
-              <Button onPress={handleReportPost} color="#B11B1B" fontSize="15">
-                Report
-              </Button>
-            </View>
-          </View>
-        </View>
-      </Modal>
     </Card>
   );
 }
